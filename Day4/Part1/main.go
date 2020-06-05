d14df6265ee26b62c4cfb5ef9df90ea0b603d834
falkn <k.fraissine@gmail.com>
2020-06-05 13:09:56 +0200

    Part1

---
+++ Day4/Part1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import ( 
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func ConvertIntToArray(n int) ([]int) {
+	var ari []int
+
+	s   := strconv.Itoa(n)
+	ars := strings.Split(s, "")
+	for _, i := range ars {
+		ie, _ := strconv.Atoi(i)
+		ari = append(ari, ie)
+	}
+
+	return ari
+}
+
+func main() {
+	var match []int
+
+	for input := 246540; input <= 787419; input++ {
+		ari := ConvertIntToArray(input)
+
+		prev := 0
+		dup  := false
+		for i, v := range ari {
+			if (prev > v) { break }
+			if (prev == v) { dup = true }
+
+			if (i == 5 && dup == true) { match = append(match, input) }
+
+			prev = v
+		}
+	}
+
+	fmt.Println(len(match))
+}
